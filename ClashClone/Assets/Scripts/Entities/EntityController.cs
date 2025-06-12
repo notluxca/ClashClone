6c22aad5876a551f391ff35752053c12cc6ad047
Lucas Fernandes <84829810+notluxca@users.noreply.github.com>
2025-06-12 12:36:12 -0300

    :update: Tank Implemented

--- ClashClone/Assets/Scripts/Entities/EntityController.cs
+++ ClashClone/Assets/Scripts/Entities/EntityController.cs
@@ -60,19 +60,12 @@ public class EntityController : MonoBehaviour
     private void UpdateAnimatorState()
     {
         if (navigationController == null || animator == null) return;
-
-        // AQUI ESTÁ A MUDANÇA: A definição de "movimento" agora é mais robusta.
-        // A entidade é considerada "em movimento" se ela AINDA NÃO CHEGOU ao seu destino.
         bool isMoving = !navigationController.IsStoppedAtDestination;
-
-        // --- LÓGICA SEM ALTERAÇÕES ---
-
-        // 1. Se a entidade está se movendo, toque a animação de movimento.
+        
         if (isMoving)
         {
             PlayAnimation(moveAnimationName);
         }
-        // 2. Se a entidade está PARADA...
         else
         {
             // Verificamos se uma animação de ataque já está tocando.
@@ -89,13 +82,10 @@ public class EntityController : MonoBehaviour
             if (navigationController.IsStoppedAtDestination && Time.time >= lastAttackTime + attackCooldown || 
             targetingController.hasReachedBase && Time.time >= lastAttackTime + attackCooldown)
             {
-                Debug.Log("Solicitando ataque caralho");
-                // Sim, então atacamos.
                 lastAttackTime = Time.time;
                 PlayAnimation(attackAnimationName);
                 
             }
-            // Se não estamos atacando e não podemos atacar (em cooldown), ficamos parados.
             else
             {
                 PlayAnimation(idleAnimationName);
